76b608a64e06e441b54bac0632ceb35a6949fa08
eijdam <adamlevan123@gmail.com>
2024-11-28 17:26:56 +0100

    Prisma

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -3,8 +3,11 @@
 
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./prisma"
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
@@ -12,6 +15,7 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  debug: true, // Enable/disable detailed logging
   pages: {
     signIn: '/auth/prihlasenie',
     signOut: '/auth/odhlasenie',
@@ -21,5 +25,5 @@ export const authOptions: NextAuthOptions = {
       // Redirect to home page after sign-in
       return baseUrl || url; // baseUrl is automatically set from NEXTAUTH_URL in .env
     },
-  },  
+  },
 };
